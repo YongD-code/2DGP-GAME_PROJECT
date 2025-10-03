4d7abcce146b3c06be85a07e0157c1751e03b666
KIM YONG HYUN <dydrkwl@gmail.com>
2025-10-03 17:53:21 +0900

    기본 함수 작성

---
+++ 2DGP - GAME_PROJECT.py
@@ -0,0 +1,41 @@
+from pico2d import *
+import random
+
+def handle_events():
+    global running
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            running = False
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            running = False
+            
+            
+open_canvas()
+
+
+def reset_world():
+    pass
+
+
+reset_world()
+
+
+def update_world():
+    pass
+
+
+def render_world():
+    pass
+
+
+while running:
+    handle_events()
+    update_world()
+    render_world()
+    delay(0.05)
+
+
+
+close_canvas()
+
