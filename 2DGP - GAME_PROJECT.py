53a3b701486f001ab10111abf1cfcb6a71426010
KIM YONG HYUN <dydrkwl@gmail.com>
2025-10-13 22:41:19 +0900

    배경 띄우기 기본 틀 구현

--- 2DGP - GAME_PROJECT.py
+++ 2DGP - GAME_PROJECT.py
@@ -1,6 +1,16 @@
 from pico2d import *
 import random
 
+class Background:
+    def __init__(self):
+        self.image = load_image('background.png')
+
+    def update(self):
+        pass
+
+    def draw(self):
+        self.image.draw(640, 360)
+
 def handle_events():
     global running
     events = get_events()
@@ -11,10 +21,13 @@ def handle_events():
             running = False
             
             
-open_canvas()
+open_canvas(1280, 720)
 
 
 def reset_world():
+    global running, background
+    running = True
+    background = Background()
     pass
 
 
@@ -26,6 +39,9 @@ def update_world():
 
 
 def render_world():
+    clear_canvas()
+    background.draw()
+    update_canvas()
     pass
 
 
