37f138662127eb24af467b805f467e55b898ba47
KIM YONG HYUN <dydrkwl@gmail.com>
2025-10-15 23:17:23 +0900

    객체들을 위한 char 추가

--- state_machine.py
+++ state_machine.py
@@ -1,13 +1,14 @@
 class StateMachine:
-    def __init__(self,start_state):
+    def __init__(self,start_state,char):
         self.current_state = start_state
-        self.current_state.enter()
+        self.current_state.enter(self.char)
+        self.char = char
         pass
 
     def update(self):
-        self.current_state.update()
+        self.current_state.update(self.char)
         pass
 
     def draw(self):
-        self.current_state.draw()
+        self.current_state.draw(self.char)
         pass
