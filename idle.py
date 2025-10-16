6a27ae8de12153d778f4d13435c6332c6b1c71ca
KIM YONG HYUN <dydrkwl@gmail.com>
2025-10-16 23:13:21 +0900

    상태 머신에 맞게 코드 수정

--- idle.py
+++ idle.py
@@ -1,6 +1,9 @@
 from pico2d import *
 
 class Idle:
+    def __init__(self, player):
+        self.player = player
+
     def enter(self,event):
         self.image = load_image('_Idle.png')
         self.frame = 0
@@ -12,4 +15,4 @@ class Idle:
         self.frame = (self.frame + 1) % 10
 
     def draw(self):
-        self.image.clip_draw(self.frame * self.w, 0, self.w, self.h, self.x, self.y, self.w * 3,self.h * 3)
+        self.image.clip_draw(self.frame * self.player.w, 0, self.player.w, self.player.h, self.player.x, self.player.y, self.player.w * 3,self.player.h * 3)
