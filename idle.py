995744e536d685a552a58855655626da38f738c2
KIM YONG HYUN <dydrkwl@gmail.com>
2025-10-17 00:21:17 +0900

    왼쪽 오른쪽 idle 구현

--- idle.py
+++ idle.py
@@ -3,10 +3,18 @@ from pico2d import *
 class Idle:
     def __init__(self, player):
         self.player = player
+        self.image_right = load_image('_Idle.png')
+        self.image_left = load_image('_Idle_R.png')
+        self.image = self.image_right
+        self.frame = 0
 
     def enter(self,event):
-        self.image = load_image('_Idle.png')
         self.frame = 0
+
+        if self.player.dir == 1:
+            self.image = self.image_right
+        elif self.player.dir == -1:
+            self.image = self.image_left
 
     def exit(self,event):
         pass
