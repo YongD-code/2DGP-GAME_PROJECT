4f8da8b018e01a911b500e41efcd4924c8adee43
KIM YONG HYUN <dydrkwl@gmail.com>
2025-10-17 00:17:59 +0900

    왼쪽 오른쪽 달리기 구현

--- run.py
+++ run.py
@@ -1,12 +1,35 @@
 from pico2d import *
 
+def right_down(e):
+    return e[0] == 'INPUT' and e[1].type == SDL_KEYDOWN and e[1].key == SDLK_RIGHT
+def right_up(e):
+    return e[0] == 'INPUT' and e[1].type == SDL_KEYUP and e[1].key == SDLK_RIGHT
+def left_down(e):
+    return e[0] == 'INPUT' and e[1].type == SDL_KEYDOWN and e[1].key == SDLK_LEFT
+def left_up(e):
+    return e[0] == 'INPUT' and e[1].type == SDL_KEYUP and e[1].key == SDLK_LEFT
+
 
 class Run:
     def __init__(self,player):
         self.player = player
-    def enter(self,event):
-        self.image = load_image('_Run.png')
+        self.image_right = load_image('_Run.png')
+        self.image_left = load_image('_Run_R.png')
+        self.image = self.image_right
         self.frame = 0
+
+    def enter(self,event):
+        self.frame = 0
+
+        if right_down(event) or left_up(event):
+            self.player.dir = 1
+        elif left_down(event) or right_up(event):
+            self.player.dir = -1
+
+        if self.player.dir == 1:
+            self.image = self.image_right
+        elif self.player.dir == -1:
+            self.image = self.image_left
 
     def exit(self,event):
         pass
@@ -14,6 +37,7 @@ class Run:
     def do(self):
         self.frame = (self.frame + 1) % 10
         self.player.x += 15 * self.player.dir
+
         if self.player.x > 1240:
             self.player.dir = 0
         if self.player.x < 40:
