a40cdf2fd02c62b280d77ff594b8522891623de9
KIM YONG HYUN <dydrkwl@gmail.com>
2025-10-19 18:53:57 +0900

    class Npc 코드 작성

---
+++ NPC.py
@@ -0,0 +1,20 @@
+from pico2d import *
+
+class Npc:
+    def __init__(self):
+        self.frames = [load_image(f'NPC_Idle{i}.png') for i in range(1,10)]
+        self.frame_index = 0
+        self.x, self.y = 1100, 180
+        self.w,self.h = 64,64
+        self.timer = 0
+        self.timer_delay = 0.15
+
+    def update(self):
+        self.timer += get_time()
+        if self.timer > self.timer_delay:
+            self.frame_index = (self.frame_index + 1) % 9
+            self.timer = 0
+
+    def draw(self):
+        self.frames[self.frame_index].draw(self.x, self.y, self.w*2.5,self.h*2.5)
+
