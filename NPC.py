ae37d2ae58bf1c39bf7279922b811b60851430ed
KIM YONG HYUN <dydrkwl@gmail.com>
2025-10-20 22:53:59 +0900

    플레이어의 위치에 따라 NPC가 바라보는 방향 변하는 코드 작성

--- NPC.py
+++ NPC.py
@@ -2,19 +2,29 @@ from pico2d import *
 
 class Npc:
     def __init__(self):
-        self.frames = [load_image(f'NPC_Idle{i}.png') for i in range(1,10)]
+        self.right_frames = [load_image(f'NPC_Idle{i}.png') for i in range(1,10)]
+        self.left_frames = [load_image(f'NPC_Idle{i}_R.png') for i in range(1,10)]
         self.frame_index = 0
         self.x, self.y = 1100, 180
         self.w,self.h = 64,64
         self.timer = 0
         self.timer_delay = 0.15
+        self.face = True
 
-    def update(self):
+    def update(self,player_x = None):
+        if player_x is not None:
+            if player_x > self.x:
+                self.face = True
+            else:
+                self.face = False
+
         self.timer += get_time()
         if self.timer > self.timer_delay:
             self.frame_index = (self.frame_index + 1) % 9
             self.timer = 0
 
     def draw(self):
-        self.frames[self.frame_index].draw(self.x, self.y, self.w*2.5,self.h*2.5)
-
+        if self.face:
+            self.right_frames[self.frame_index].draw(self.x, self.y, self.w*2.5,self.h*2.5)
+        else:
+            self.left_frames[self.frame_index].draw(self.x, self.y, self.w*2.5,self.h*2.5)
