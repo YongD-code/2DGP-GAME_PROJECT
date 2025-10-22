f0a702794b18c4094ae7ee8af5b66bf58f2e8145
KIM YONG HYUN <dydrkwl@gmail.com>
2025-10-22 22:12:35 +0900

    class Harvest 추가 및 코드 작성

---
+++ harvest.py
@@ -0,0 +1,33 @@
+from pico2d import *
+
+def down_down(e):
+    return e[0] == 'INPUT' and e[1].type == SDL_KEYDOWN and e[1].key == SDLK_DOWN
+def down_up(e):
+    return e[0] == 'INPUT' and e[1].type == SDL_KEYUP and e[1].key == SDLK_DOWN
+
+class Harvest:
+    def __init__(self,player):
+        self.player = player
+        self.image_right = load_image('harvest.png')
+        self.image_left = load_image('harvest_R.png')
+        self.image = self.image_right
+        self.frame = 0
+
+    def enter(self,event):
+        self.frame = 0
+        if self.player.dir == 1:
+            self.image = self.image_right
+        elif self.player.dir == -1:
+            self.image = self.image_left
+
+    def exit(self,event):
+        pass
+
+    def do(self):
+        if self.frame < 2:
+            self.frame += 1
+        else:
+            self.frame = 2
+
+    def draw(self):
+        self.image.clip_draw(self.frame * self.player.w, 0, self.player.w, self.player.h, self.player.x, self.player.y, self.player.w * 3,self.player.h * 3)
