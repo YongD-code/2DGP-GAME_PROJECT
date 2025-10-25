e923eef2b540eff30630b7493fbe416a20694991
KIM YONG HYUN <dydrkwl@gmail.com>
2025-10-25 23:58:03 +0900

    점프 코드 추가

--- player.py
+++ player.py
@@ -79,3 +79,31 @@ class Roll:
     def draw(self):
         self.image.clip_draw(self.frame * self.player.w, 0, self.player.w, self.player.h, self.player.x, self.player.y, self.player.w * 3,self.player.h * 3)
 
+class jump:
+    def __init__(self,player):
+        self.player = player
+        self.image_right = load_image('jump.png')
+        self.image_left = load_image('jump_R.png')
+        self.image = self.image_right
+        self.frame = 0
+
+    def enter(self,event):
+        self.frame = 0
+
+        if self.player.dir == 1:
+            self.image = self.image_right
+        elif self.player.dir == -1:
+            self.image = self.image_left
+
+    def exit(self,event):
+        pass
+
+    def do(self):
+        if self.frame < 5:
+            self.frame += 1
+        else:
+            self.frame = 5
+
+    def draw(self):
+        self.image.clip_draw(self.frame * self.player.w, 0, self.player.w, self.player.h, self.player.x, self.player.y, self.player.w * 3,self.player.h * 3)
+
