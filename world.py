45b38d54d3c96d6c2853103470496996f87fd35c
KIM YONG HYUN <dydrkwl@gmail.com>
2025-10-29 22:24:35 +0900

    게임 월드로 묶기 위한 world.py 추가

---
+++ world.py
@@ -0,0 +1,40 @@
+# layer 0 = 배경
+# layer 1 = 건물, 지형
+# layer 2 = 캐릭터, NPC, 포탈
+# layer 3 = UI
+world = [[],[],[],[]]
+crops = []
+
+player = None
+background = None
+ground = None
+blacksmith = None
+house = None
+portal = None
+npc = None
+gametime = None
+
+def add_object(o, layer):
+    world[layer].append(o)
+
+def update(frame_time): #업데이트하는 기능
+    for layer in world:
+        for o in layer:
+            try:
+                o.update(frame_time)
+            except TypeError:
+                o.update()
+
+def render(): #그리는 기능
+    for layer in world:
+        for o in layer:
+            o.draw()
+
+def remove_object(o, layer):
+    for layer in world:
+        if o in layer:
+            layer.remove(o)
+            return
+
+
+
