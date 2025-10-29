83881c4a308935b608cbca72e6cd3b3bd7d8aaf9
KIM YONG HYUN <dydrkwl@gmail.com>
2025-10-29 21:55:36 +0900

    작물과의 연계 코드 추가

--- harvest.py
+++ harvest.py
@@ -1,4 +1,5 @@
 from pico2d import *
+from crop import Crop
 
 def down_down(e):
     return e[0] == 'INPUT' and e[1].type == SDL_KEYDOWN and e[1].key == SDLK_DOWN
@@ -12,6 +13,8 @@ class Harvest:
         self.image_left = load_image('harvest_R.png')
         self.image = self.image_right
         self.frame = 0
+        self.action = None
+        self.check_crop = None
 
     def enter(self,event):
         self.frame = 0
@@ -20,14 +23,28 @@ class Harvest:
         elif self.player.dir == -1:
             self.image = self.image_left
 
+        self.check_crop = self.player.find_crop()
+
+        if self.check_crop:
+            self.action = 'harvest'
+        else:
+            self.action = 'plant'
+
     def exit(self,event):
+        self.action = None
+        self.check_crop = None
         pass
 
     def do(self):
-        if self.frame < 2:
-            self.frame += 1
-        else:
-            self.frame = 2
+        global crops
+
+        self.frame += 1
+        if self.frame >= 3:
+            if self.action == 'harvest' and self.check_crop:
+                self.check_crop.harvest()
+            elif self.action == 'plant':
+                crops.append(Crop(self.player.x, 96))
+            self.player.state_machine.change_state(self.player.IDLE)
 
     def draw(self):
         self.image.clip_draw(self.frame * self.player.w, 0, self.player.w, self.player.h, self.player.x, self.player.y, self.player.w * 3,self.player.h * 3)
