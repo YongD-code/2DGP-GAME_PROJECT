5b27bf30b64d20bd653c4e7fbf2b2feb6692fac3
KIM YONG HYUN <dydrkwl@gmail.com>
2025-10-30 19:11:23 +0900

    스프라이트 순서 수정 및 테스트

--- crop.py
+++ crop.py
@@ -4,8 +4,8 @@ class Crop:
     def __init__(self,x,y):
         self.image = load_image('crop.png')
         self.x,self.y = x,y
-        self.stage = 0
-        self.max_stage = 3
+        self.stage = 4
+        self.max_stage = 1
         self.timer = 0
         self.harvested = False
 
@@ -14,10 +14,13 @@ class Crop:
 
     def update(self,frame_time):
         self.timer += frame_time
-        if self.timer > 5.0 and self.stage < self.max_stage:
-            self.stage += 1
+        if self.timer > 5.0 and self.stage > self.max_stage:
+            self.stage -= 1
             self.timer = 0
 
     def draw(self):
         if not self.harvested:
-            self.image.clip_draw(self.stage * 32, 0, 32, 32, self.x, self.y, 64, 64)
+            if self.stage >= 3:
+                self.image.clip_draw(self.stage * 16, 0, 16, 16, self.x, self.y, 32, 32)
+            else:
+                self.image.clip_draw(self.stage * 16, 0, 16, 16, self.x, 132, 48, 48)
