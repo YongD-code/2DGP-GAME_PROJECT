e866f7919e35d2bbb474f76f951d76afd1ad5eb5
KIM YONG HYUN <dydrkwl@gmail.com>
2025-11-07 18:59:02 +0900

    던전 타일을 사용할 맵 클래스 추가

--- dungeon_tile.py
+++ dungeon_tile.py
@@ -13,3 +13,25 @@ class DungeonTile:
         left = self.col * self.w
         bottom = self.image.h - (self.row + 1) * self.h
         self.image.clip_draw(left, bottom, self.w, self.h, self.x, self.y,self.w*2,self.h*2)
+
+class DungeonMap:
+    def __init__(self):
+        self.tiles = []
+        self.make_tiles()
+
+    def make_tiles(self):
+        for i in range(0,21):
+            self.tiles.append(DungeonTile(32+i*32,80,0,8))
+
+        for i in range(5, 10):
+            self.tiles.append(DungeonTile(32 + i * 32, 200, 1, 8))
+
+        for i in range(12, 16):
+            self.tiles.append(DungeonTile(32 + i * 32, 320, 2, 8))
+
+    def draw(self):
+        for t in self.tiles:
+            t.draw()
+
+    def get_tiles(self):
+        return self.tiles
