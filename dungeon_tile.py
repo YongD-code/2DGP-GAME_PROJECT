418a12269ebfe2128a01c7b499d426f8e32a5c7c
KIM YONG HYUN <dydrkwl@gmail.com>
2025-11-07 19:13:31 +0900

    타일 위치 및 크기 재조정

--- dungeon_tile.py
+++ dungeon_tile.py
@@ -11,11 +11,11 @@ class DungeonTile:
 
     def update(self):
         pass
-    
+
     def draw(self):
         left = self.col * self.w
         bottom = self.image.h - (self.row + 1) * self.h
-        self.image.clip_draw(left, bottom, self.w, self.h, self.x, self.y,self.w*2,self.h*2)
+        self.image.clip_draw(left, bottom, self.w, self.h, self.x, self.y,self.w*2.5,self.h*2.5)
 
 class DungeonMap:
     def __init__(self):
@@ -24,13 +24,13 @@ class DungeonMap:
 
     def make_tiles(self):
         for i in range(0,21):
-            self.tiles.append(DungeonTile(32+i*32,80,0,8))
+            self.tiles.append(DungeonTile(86+i*32,240,1,2))
 
-        for i in range(5, 10):
-            self.tiles.append(DungeonTile(32 + i * 32, 200, 1, 8))
+        for i in range(29, 38):
+            self.tiles.append(DungeonTile(16 + i * 32, 340, 1, 2))
 
-        for i in range(12, 16):
-            self.tiles.append(DungeonTile(32 + i * 32, 320, 2, 8))
+        for i in range(0, 16):
+            self.tiles.append(DungeonTile(86 + i * 32, 470, 1, 2))
 
     def update(self):
         pass
