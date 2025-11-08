e2a75030c74a96dafdb71b958db80b0a29948018
KIM YONG HYUN <dydrkwl@gmail.com>
2025-11-09 01:04:12 +0900

    슬라임이 플레이어를 따라다니도록 수정

--- slime.py
+++ slime.py
@@ -1,10 +1,17 @@
 from pico2d import *
+import math
 
 class Slime:
-    def __init__(self, x, y,color = 'blue'):
-        self.blue_slime = load_image('blue_slime.png')
+    DETECT_RADIUS   = 260.0
+    STOP_DIST       = 24.0
+    IDLE_FRAME_TIME = 0.15
+    MOVE_FRAME_TIME = 0.10
+    MOVE_SPEED      = 110.0
+
+    def __init__(self, x, y, color='blue'):
+        self.blue_slime  = load_image('blue_slime.png')
         self.green_slime = load_image('green_slime.png')
-        self.red_slime = load_image('red_slime.png')
+        self.red_slime   = load_image('red_slime.png')
 
         self.cols = 7
         self.rows = 6
@@ -13,13 +20,15 @@ class Slime:
 
         self.x, self.y = x, y
         self.dir = -1
-        self.speed = 50
+        self.speed = Slime.MOVE_SPEED
         self.frame = 0
         self.action = 0
         self.time_acc = 0.0
 
-        self.idle_frame_count = 4
         self.idle_row = 6
+        self.idle_frame_count = 4
+        self.move_row = 5
+        self.move_frame_count = 5
 
         self.sheet = None
         self.set_color(color)
@@ -34,17 +43,28 @@ class Slime:
             self.sheet = self.blue_slime
 
     def update(self, frame_time):
-        self.time_acc += frame_time
-        if self.time_acc > 0.15:
-            if self.action == 0:
-                self.frame = (self.frame + 1) % self.idle_frame_count
+        try:
+            from world import player, left_boundary, right_boundary
+        except ImportError:
+            player = None
+            left_boundary, right_boundary = 0, 999999
+
+        if player is not None:
+            same_floor = abs(player.y - self.y) <= 100
+            if same_floor:
+                dx = player.x - self.x
+                if abs(dx) <= Slime.DETECT_RADIUS:
+                    self.action = 1
+                    if abs(dx) > Slime.STOP_DIST:
+                        self.dir = 1 if dx > 0 else -1
+                        self.x += self.dir * self.speed * frame_time
+                else:
+                    self.action = 0
             else:
-                self.frame = (self.frame + 1) % self.cols
-            self.time_acc = 0.0
+                self.action = 0
+        else:
+            self.action = 0
 
-        self.action = 0
-
-        from world import left_boundary, right_boundary
         if self.x < left_boundary + 40:
             self.x = left_boundary + 40
             self.dir = 1
@@ -52,13 +72,32 @@ class Slime:
             self.x = right_boundary - 40
             self.dir = -1
 
-    def draw(self):
-        row = 6
+        self.time_acc += frame_time
+        if self.action == 0:
+            if self.time_acc >= Slime.IDLE_FRAME_TIME:
+                self.frame = (self.frame + 1) % self.idle_frame_count
+                self.time_acc = 0.0
+        else:
+            if self.time_acc >= Slime.MOVE_FRAME_TIME:
+                self.frame = (self.frame + 1) % self.move_frame_count
+                self.time_acc = 0.0
 
-        x_clip = int(self.frame) * self.w
+    def draw(self):
+        if self.action == 0:
+            row = self.idle_row
+            frame_count = self.idle_frame_count
+        else:
+            row = self.move_row
+            frame_count = self.move_frame_count
+
+        idx = int(self.frame) % frame_count
+        x_clip = idx * self.w
         y_clip = (self.rows + row) * self.h
 
+        draw_w = self.w * 3
+        draw_h = self.h * 3
+
         if self.dir == 1:
-            self.sheet.clip_draw(x_clip, y_clip,self.w, self.h,self.x, self.y,self.w*3, self.h*3)
+            self.sheet.clip_draw(x_clip, y_clip, self.w, self.h,self.x, self.y, draw_w, draw_h)
         else:
-            self.sheet.clip_composite_draw(x_clip, y_clip,self.w, self.h,0, 'h',self.x, self.y, self.w*3, self.h*3)
+            self.sheet.clip_composite_draw(x_clip, y_clip, self.w, self.h, 0, 'h', self.x, self.y, draw_w, draw_h)
