af595039093331c228c9fa894ea0a9a4dc6bc5b4
KIM YONG HYUN <dydrkwl@gmail.com>
2025-11-10 03:42:26 +0900

    로딩 모드 코드 작성

--- loading_mode.py
+++ loading_mode.py
@@ -1,3 +1,50 @@
 from pico2d import *
 import game_framework
+import dungeon_mode,world, GAME_PROJECT
+next_stage = 1
+loading = 1.0
+
+load_timer = 0.0
+
+def start(target_stage: int = 1):
+
+    global next_stage, loading
+    next_stage = target_stage
+
+def init():
+    global load_timer,image
+    load_timer = 0.0
+    pass
+
+def finish():
+    pass
+
+def update():
+    global load_timer, loading
+    load_timer += game_framework.frame_time
+
+    if load_timer >= loading:
+        try:
+            dungeon_mode.init(stage=next_stage)
+        except TypeError:
+            dungeon_mode.init()
+        game_framework.change_mode(dungeon_mode)
+
+def handle_events():
+    events = get_events()
+    for e in events:
+        if e.type == SDL_QUIT:
+            game_framework.quit()
+        elif e.type == SDL_KEYDOWN and e.key == SDLK_ESCAPE:
+            game_framework.change_mode(GAME_PROJECT)
+        else:
+            world.player.handle_event(e)
+
+def draw():
+    clear_canvas()
+    update_canvas()
+
+def pause():  pass
+def resume(): pass
+
 
