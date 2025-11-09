11b7e5e1ab7aa253fc6c25dfbb854aabb96a1261
KIM YONG HYUN <dydrkwl@gmail.com>
2025-11-10 02:24:01 +0900

    피격 클래스 추가

---
+++ hit.py
@@ -0,0 +1,50 @@
+from pico2d import *
+import world, game_framework
+
+class Hit:
+    def __init__(self, player):
+        self.player = player
+        self.hit_image = load_image('hit.png')      # dir == 1
+        self.image = self.hit_image
+        self.timer = 0.0
+        self.knock_timer = 0.0
+        self.knock_dir = 0
+
+    def enter(self, event):
+        self.image = self.hit_image
+        self.timer = 0.30         # 히트 이미지 표시 시간
+        self.knock_timer = 0.12   # 넉백 유지 시간
+        self.knock_dir = -self.player.dir  # 반대방향 넉백
+
+    def exit(self, event):
+        pass
+
+    def do(self):
+        dt = game_framework.frame_time
+
+        # 짧은 넉백
+        if self.knock_timer > 0.0:
+            self.player.x += self.knock_dir * 200.0 * dt  # 넉백 속도
+            self.knock_timer -= dt
+
+        # 표시 시간 끝나면 복귀
+        self.timer -= dt
+        if self.timer <= 0.0:
+            if self.player.right_input or self.player.left_input:
+                self.player.state_machine.change_state(self.player.RUN)
+            else:
+                self.player.state_machine.change_state(self.player.IDLE)
+
+        # 경계 보정
+        if self.player.x > world.right_boundary:
+            self.player.x = world.right_boundary
+        if self.player.x < world.left_boundary:
+            self.player.x = world.left_boundary
+
+    def draw(self):
+        # dir이 1이면 그냥 draw, -1이면 좌우반전 composite_draw
+        if self.player.dir == 1:
+            self.image.draw(self.player.x, self.player.y,self.player.w * 3, self.player.h * 3)
+        else:
+            self.image.composite_draw(0, 'h', self.player.x, self.player.y,self.player.w * 3, self.player.h * 3)
+
