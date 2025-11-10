ccec1378bbd0122b6448c8ad4cecf107b2d3d999
KIM YONG HYUN <dydrkwl@gmail.com>
2025-11-10 22:13:51 +0900

    사용하는 타일 모양 수정

--- dungeon_tile.py
+++ dungeon_tile.py
@@ -19,7 +19,7 @@ class DungeonTile:
         bottom = self.image.h - (self.row + 1) * self.h
         self.image.clip_draw(left, bottom, self.w, self.h, self.x, self.y,self.w * SCALE,self.h * SCALE)
         #바운딩 박스 확인용 나중에 삭제할 예정
-        draw_rectangle(*self.get_bb())
+        #draw_rectangle(*self.get_bb())
 
     def get_bb(self):
         half_w = (self.w * SCALE) / 2
@@ -39,20 +39,20 @@ class DungeonMap:
 
     def make_tiles_1(self):
         for i in range(0,21):
-            self.tiles.append(DungeonTile(86+i*32,240,1,2))
+            self.tiles.append(DungeonTile(86+i*32,240,2,1))
 
         for i in range(29, 38):
-            self.tiles.append(DungeonTile(16 + i * 32, 340, 1, 2))
+            self.tiles.append(DungeonTile(16 + i * 32, 340, 2, 1))
 
         for i in range(0, 16):
-            self.tiles.append(DungeonTile(86 + i * 32, 470, 1, 2))
+            self.tiles.append(DungeonTile(86 + i * 32, 470, 2, 1))
 
     def make_tiles_2(self):
         for i in range(0,21):
-            self.tiles.append(DungeonTile(86+i*32,240,1,2))
+            self.tiles.append(DungeonTile(86+i*32,240,2,1))
 
         for i in range(18, 38):
-            self.tiles.append(DungeonTile(16 + i * 32, 440, 1, 2))
+            self.tiles.append(DungeonTile(16 + i * 32, 440, 2, 1))
         pass
 
     def make_tiles_3(self):
