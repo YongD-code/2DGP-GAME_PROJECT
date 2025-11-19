7a6a976ee3f6c9d9f4beb74d8526eb69c999cbb7
KIM YONG HYUN <dydrkwl@gmail.com>
2025-11-19 23:35:21 +0900

    씨앗 종류에 따라 다른 작물이 자라도록 수정

--- crop.py
+++ crop.py
@@ -1,5 +1,12 @@
 from pico2d import *
 
+CROP_TYPE = {
+    "corn": 0,
+    "pumpkin": 1,
+    "potato": 2,
+    "strawberry": 3
+}
+
 class Crop:
     def __init__(self,x,y,crop_type="corn"):
         self.image = load_image('crop.png')
@@ -21,7 +28,10 @@ class Crop:
 
     def draw(self):
         if not self.harvested:
+            row = CROP_TYPE[self.crop_type]
+            sy = row * 16
+
             if self.stage >= 3:
-                self.image.clip_draw(self.stage * 16, 0, 16, 16, self.x, self.y+10, 32, 32)
+                self.image.clip_draw(self.stage * 16, sy, 16, 16, self.x, self.y+10, 32, 32)
             else:
-                self.image.clip_draw(self.stage * 16, 0, 16, 16, self.x, 132, 48, 48)
+                self.image.clip_draw(self.stage * 16, sy, 16, 16, self.x, 132, 48, 48)
