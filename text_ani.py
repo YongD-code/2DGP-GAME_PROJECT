cfaf5206116634c2ea27cb8dab0f126b683f041e
KIM YONG HYUN <dydrkwl@gmail.com>
2025-11-19 23:52:01 +0900

    텍스트 코드 작성

--- text_ani.py
+++ text_ani.py
@@ -1,1 +1,23 @@
 from pico2d import *
+
+class TextAni:
+    def __init__(self, x, y, text, color=(255,255,255)):
+        self.x = x
+        self.y = y
+        self.text = text
+        self.alpha = 1.0
+        self.timer = 0
+        self.font = load_font('D2Coding-Ver1.3.2-20180524.ttf', 20)
+        self.color = color
+
+    def update(self, frame_time):
+        self.timer += frame_time
+        self.y += 30 * frame_time
+        self.alpha -= 0.7 * frame_time
+
+        if self.alpha < 0:
+            self.alpha = 0
+
+    def draw(self):
+        r, g, b = int(self.color[0] * self.alpha), int(self.color[1] * self.alpha), int(self.color[2] * self.alpha)
+        self.font.draw(self.x, self.y, self.text, (r, g, b))
