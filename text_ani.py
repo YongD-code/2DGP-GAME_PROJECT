9cdeee6191a080a4df3bcd0fd175862cc6a0ec3e
KIM YONG HYUN <dydrkwl@gmail.com>
2025-11-24 15:02:27 +0900

    데미지 텍스트 추가

--- text_ani.py
+++ text_ani.py
@@ -65,3 +65,34 @@ class DeathTextAni:
         b = int(self.color[2] * self.alpha)
 
         self.font.draw(self.x, self.y, self.text, (r, g, b))
+
+class DamageTextAni:
+    def __init__(self, x, y, damage, color=(255, 80, 80)):
+        self.x = x
+        self.y = y
+        self.text = str(damage)
+        self.alpha = 1.0
+        self.timer = 0
+        self.duration = 0.9
+        self.font = load_font('D2Coding-Ver1.3.2-20180524.ttf', 28)
+        self.color = color
+
+    def update(self, frame_time):
+        self.timer += frame_time
+        self.y += 60 * frame_time  # 더 빠르게 위로
+
+        if self.timer > self.duration * 0.4:
+            self.alpha -= (frame_time / (self.duration * 0.6))
+            if self.alpha < 0:
+                self.alpha = 0
+
+    def text_dead(self):
+        return self.alpha <= 0
+
+    def draw(self):
+        r = int(self.color[0] * self.alpha)
+        g = int(self.color[1] * self.alpha)
+        b = int(self.color[2] * self.alpha)
+
+        self.font.draw(self.x - 10, self.y, self.text, (r, g, b))
+
