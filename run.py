e4a2750e2cffb820af7b9df147faf65819968f0b
KIM YONG HYUN <dydrkwl@gmail.com>
2025-11-28 23:48:56 +0900

    RUN FRAME TIME 적용

--- run.py
+++ run.py
@@ -10,6 +10,7 @@ RUN_SPEED_MPM = (RUN_SPEED_KMPH * 1000.0 / 60.0)
 RUN_SPEED_MPS = (RUN_SPEED_MPM / 60.0)
 RUN_SPEED_PPS = (RUN_SPEED_MPS * PIXEL_PER_METER)
 
+FPS_RUN = 12
 
 def right_down(e):
     return e[0] == 'INPUT' and e[1].type == SDL_KEYDOWN and e[1].key == SDLK_RIGHT
@@ -28,29 +29,23 @@ class Run:
         self.image_left = load_image('_Run_R.png')
         self.image = self.image_right
         self.frame = 0
+        self.total_frames = 10
 
     def enter(self,event):
-        frame_time = game_framework.frame_time
-        self.frame = (self.frame + 1) % 10
-        self.player.x += RUN_SPEED_PPS * frame_time * self.player.dir
-
         if right_down(event) or left_up(event):
             self.player.dir = 1
         elif left_down(event) or right_up(event):
             self.player.dir = -1
 
-        # if self.player.dir == 1:
-        #     self.image = self.image_right
-        # elif self.player.dir == -1:
-        #     self.image = self.image_left
-
     def exit(self,event):
         pass
 
     def do(self):
-        self.frame = (self.frame + 1) % 10
-
         self.player.x += RUN_SPEED_PPS* game_framework.frame_time * self.player.dir
+
+        self.frame += FPS_RUN * game_framework.frame_time
+        if self.frame >= self.total_frames:
+            self.frame -= self.total_frames
 
         if self.player.x > world.right_boundary:
             self.player.x = world.right_boundary
@@ -58,7 +53,8 @@ class Run:
             self.player.x = world.left_boundary
 
     def draw(self):
+        frame = int(self.frame)
         if self.player.dir == 1:
-            self.image.clip_draw(self.frame * self.player.w, 0, self.player.w, self.player.h,self.player.x, self.player.y, self.player.w * 3, self.player.h * 3)
+            self.image.clip_draw(frame * self.player.w, 0, self.player.w, self.player.h,self.player.x, self.player.y, self.player.w * 3, self.player.h * 3)
         else :
-            self.image.clip_composite_draw(self.frame * self.player.w, 0, self.player.w, self.player.h,0, 'h',self.player.x, self.player.y, self.player.w * 3, self.player.h * 3)
+            self.image.clip_composite_draw(frame * self.player.w, 0, self.player.w, self.player.h,0, 'h',self.player.x, self.player.y, self.player.w * 3, self.player.h * 3)
